b2f5e6ead8a51266a08b80b15f39b8aec9e469ac
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2023-09-11 14:02:47 +0600

    #2348 Front: правки в верстке

--- docs/assets/js/index.4.16089cb0.js
+++ docs/assets/js/index.4.16089cb0.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelectorAll(".doctor .doctor__btn"),t=document.querySelector(".modal"),c=document.querySelector(".modal .modal__close");e.forEach((e=>{e.addEventListener("click",(()=>{t.classList.add("active")}))})),c.addEventListener("click",(()=>{t.classList.remove("active")}))}));
-//# sourceMappingURL=index.3.beb7bac3.js.map
+//# sourceMappingURL=index.4.16089cb0.js.map
