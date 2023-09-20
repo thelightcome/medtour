a936c43d4ec9c1b2e0900cf4f88eb67753aa65f9
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2023-09-20 13:43:34 +0600

    save changes

--- docs/assets/js/index.8.0e828135.js
+++ docs/assets/js/index.8.0e828135.js
@@ -1,2 +1,2 @@
 document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelectorAll(".doctor .doctor__btn"),t=document.querySelector(".modal"),o=document.querySelector(".modal__overlay"),d=document.querySelector(".modal .modal__close");e.forEach((e=>{e.addEventListener("click",(()=>{t.classList.add("active"),document.body.style.overflow="hidden"}))})),d.addEventListener("click",(()=>{t.classList.remove("active"),document.body.style.overflow="unset"})),o.addEventListener("click",(e=>{e.target.closest(".modal__content")||t.classList.remove("active")})),o.addEventListener("wheel",(e=>{e.stopPropagation()}))}));
-//# sourceMappingURL=index.4.fd57582d.js.map
+//# sourceMappingURL=index.8.0e828135.js.map
